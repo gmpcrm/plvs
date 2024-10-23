16d0dc337f065c18b2598236be1b5aefe4eec491
Nikolay Stepankov <gmpcrm@gmail.com>
2024-10-23 16:41:28 +0200

    force orange pi arm64

--- install_local_opencv.sh
+++ install_local_opencv.sh
@@ -56,7 +56,7 @@ function get_usable_cuda_version(){
 
 export TARGET_FOLDER=Thirdparty
 
-export OPENCV_VERSION="4.10.0"   # OpenCV version to download and install. See tags in https://github.com/opencv/opencv 
+export OPENCV_VERSION="4.5.5"   # Use a compatible OpenCV version. Adjust if necessary.
 
 # ====================================================
 print_blue  "Configuring and building $TARGET_FOLDER/opencv ..."
@@ -91,64 +91,35 @@ else
         echo using CUDA $CUDA_VERSION
         CUDA_ON=ON        
     else
-        print_red "Warning: CUDA $CUDA_VERSION not found and will not be used!"
+        print_red "Warning: CUDA not found and will not be used!"
         CUDA_ON=OFF
     fi 
 fi 
 
-#forece CUDA off
+# Force CUDA off for Orange Pi unless you have CUDA installed
 CUDA_ON=OFF
 
 echo CUDA_ON: $CUDA_ON
-export PATH=/usr/local/$CUDA_VERSION/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/$CUDA_VERSION/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 # Pre-installing required packages 
 
-# Update the sources.list if Ubuntu 16.04 is detected
-if [[ $version == *"16.04"* ]] ; then
-    print_red "Warning: Ubuntu 16.04 (Xenial) is no longer supported."
-    print_blue "Modifying /etc/apt/sources.list to use old-releases.ubuntu.com"
-    sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup.$(date +%Y%m%d%H%M%S)
-    sudo sed -i 's/http:\/\/archive\.ubuntu\.com\/ubuntu\//http:\/\/old-releases.ubuntu.com\/ubuntu\//g' /etc/apt/sources.list
-    sudo sed -i 's/http:\/\/security\.ubuntu\.com\/ubuntu\//http:\/\/old-releases.ubuntu.com\/ubuntu\//g' /etc/apt/sources.list
-fi
+sudo apt-get update
 
-if [[ ! -d $TARGET_FOLDER/opencv ]]; then
-    sudo apt-get update
-    # Install base packages
-    sudo apt-get install -y pkg-config libtbb-dev libeigen3-dev
-    sudo apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev
-    sudo apt-get install -y libglew-dev libopenblas-dev
-    sudo apt-get install -y curl software-properties-common unzip
-    sudo apt-get install -y build-essential cmake 
-    sudo apt-get install -y yasm libgtk2.0-dev
+# Install base packages
+sudo apt-get install -y pkg-config libtbb-dev libeigen3-dev
+sudo apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev
+sudo apt-get install -y libglew-dev libopenblas-dev
+sudo apt-get install -y curl software-properties-common unzip
+sudo apt-get install -y build-essential cmake 
+sudo apt-get install -y yasm libgtk2.0-dev libgtk-3-dev
 
-    # Install video and audio codecs and formats
-    sudo apt-get install -y libv4l-dev libdc1394-22-dev libtheora-dev libvorbis-dev \
+# Install video and audio codecs and formats
+sudo apt-get install -y libv4l-dev libdc1394-22-dev libtheora-dev libvorbis-dev \
                             libxvidcore-dev libx264-dev \
                             libopencore-amrnb-dev libopencore-amrwb-dev libxine2-dev
 
-    # For FFmpeg support
-    DO_INSTALL_FFMPEG=$(check_package ffmpeg)
-    if [ $DO_INSTALL_FFMPEG -eq 1 ] ; then
-        echo "Installing ffmpeg and its dependencies"
-        sudo apt-get install -y ffmpeg libavcodec-dev libavformat-dev libavutil-dev libpostproc-dev libswscale-dev
-    fi
-
-    if [[ "$CUDA_ON" == "ON" ]]; then 
-        install_packages libcudnn8 libcudnn8-dev
-    fi 
-
-    if [[ $version == *"16.04"* ]] ; then
-        # Specific packages for Ubuntu 16.04
-        sudo apt-get install -y libpng12-dev libjasper-dev 
-        if [ "$(uname -m)" == "aarch64" ]; then
-            echo "Configuring for arm64 on Xenial..."
-            sudo apt-get install -y libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libv4l-dev libgtk-3-dev
-        fi
-    fi
-fi
+# For FFmpeg support
+sudo apt-get install -y ffmpeg libavcodec-dev libavformat-dev libavutil-dev libpostproc-dev libswscale-dev
 
 # Now let's download and compile OpenCV and opencv_contrib
 cd $TARGET_FOLDER
@@ -160,10 +131,10 @@ if [ ! -f opencv/install/lib/libopencv_core.so ]; then
       rm $OPENCV_VERSION.zip
       cd opencv-$OPENCV_VERSION
 
-      wget https://github.com/opencv/opencv_contrib/archive/$OPENCV_VERSION.zip
+      wget https://github.com/opencv/opencv_contrib/archive/$OPENCV_VERSION.zip -O opencv_contrib.zip
       sleep 1
-      unzip $OPENCV_VERSION.zip
-      rm $OPENCV_VERSION.zip
+      unzip opencv_contrib.zip
+      rm opencv_contrib.zip
 
       cd ..
       mv opencv-$OPENCV_VERSION opencv
@@ -175,47 +146,20 @@ if [ ! -f opencv/install/lib/libopencv_core.so ]; then
     cd build
     echo "I am in "$(pwd)
     machine="$(uname -m)"
-    if [[ "$version" == *"16.04"* && "$machine" == "aarch64" ]]; then
-        echo "Building for arm64 on Xenial"
+    if [ "$machine" == "aarch64" ] || [ "$machine" == "arm64" ]; then
+        echo "Building for arm64 on Ubuntu 20.04"
         cmake \
           -DCMAKE_BUILD_TYPE=Release \
           -DCMAKE_INSTALL_PREFIX="`pwd`/../install" \
           -DOPENCV_EXTRA_MODULES_PATH="`pwd`/../opencv_contrib-$OPENCV_VERSION/modules" \
-          -DWITH_QT=ON \
-          -DWITH_GTK=OFF \
-          -DWITH_OPENGL=ON \
+          -DWITH_QT=OFF \
+          -DWITH_GTK=ON \
+          -DWITH_OPENGL=OFF \
           -DWITH_TBB=ON \
           -DWITH_V4L=ON \
           -DWITH_FFMPEG=ON \
+          -DWITH_GSTREAMER=ON \
           -DWITH_CUDA=$CUDA_ON \
-          -DWITH_CUBLAS=$CUDA_ON \
-          -DWITH_CUFFT=$CUDA_ON \
-          -DCUDA_FAST_MATH=$CUDA_ON \
-          -DBUILD_opencv_cudacodec=OFF \
-          -DENABLE_FAST_MATH=1 \
-          -DBUILD_DOCS=OFF \
-          -DBUILD_TESTS=OFF \
-          -DBUILD_PERF_TESTS=OFF \
-          -DINSTALL_PYTHON_EXAMPLES=OFF \
-          -DINSTALL_C_EXAMPLES=OFF \
-          -DBUILD_EXAMPLES=OFF \
-          -Wno-deprecated-gpu-targets ..
-    elif [ "$machine" == "x86_64" ]; then
-        echo "Building x86_64 config"
-        cmake \
-          -DCMAKE_BUILD_TYPE=Release \
-          -DCMAKE_INSTALL_PREFIX="`pwd`/../install" \
-          -DOPENCV_EXTRA_MODULES_PATH="`pwd`/../opencv_contrib-$OPENCV_VERSION/modules" \
-          -DWITH_QT=ON \
-          -DWITH_GTK=OFF \
-          -DWITH_OPENGL=ON \
-          -DWITH_TBB=ON \
-          -DWITH_V4L=ON \
-          -DWITH_FFMPEG=ON \
-          -DWITH_CUDA=$CUDA_ON \
-          -DWITH_CUBLAS=$CUDA_ON \
-          -DWITH_CUFFT=$CUDA_ON \
-          -DCUDA_FAST_MATH=$CUDA_ON \
           -DBUILD_opencv_cudacodec=OFF \
           -DENABLE_NEON=ON \
           -DENABLE_FAST_MATH=ON \
@@ -225,7 +169,30 @@ if [ ! -f opencv/install/lib/libopencv_core.so ]; then
           -DINSTALL_PYTHON_EXAMPLES=OFF \
           -DINSTALL_C_EXAMPLES=OFF \
           -DBUILD_EXAMPLES=OFF \
-          -Wno-deprecated-gpu-targets ..
+          -DWITH_OPENCL=OFF \
+          ..
+    else
+        echo "Building default configuration"
+        cmake \
+          -DCMAKE_BUILD_TYPE=Release \
+          -DCMAKE_INSTALL_PREFIX="`pwd`/../install" \
+          -DOPENCV_EXTRA_MODULES_PATH="`pwd`/../opencv_contrib-$OPENCV_VERSION/modules" \
+          -DWITH_QT=ON \
+          -DWITH_GTK=OFF \
+          -DWITH_OPENGL=ON \
+          -DWITH_TBB=ON \
+          -DWITH_V4L=ON \
+          -DWITH_FFMPEG=ON \
+          -DWITH_CUDA=OFF \
+          -DBUILD_opencv_cudacodec=OFF \
+          -DENABLE_FAST_MATH=ON \
+          -DBUILD_DOCS=OFF \
+          -DBUILD_TESTS=OFF \
+          -DBUILD_PERF_TESTS=OFF \
+          -DINSTALL_PYTHON_EXAMPLES=OFF \
+          -DINSTALL_C_EXAMPLES=OFF \
+          -DBUILD_EXAMPLES=OFF \
+          ..
     fi
     make -j$(nproc)
     make install 
