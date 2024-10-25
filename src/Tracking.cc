d3e94521f891d0c53ffb5182b38d32d6cbae03eb
Nikolay Stepankov <gmpcrm@gmail.com>
2024-10-25 10:00:24 +0200

    SaveCameraParametersToFile

--- src/Tracking.cc
+++ src/Tracking.cc
@@ -206,7 +206,7 @@ struct DefaultParameters {
 
 static DefaultParameters defaults;
 
-void SaveCameraParametersToFile(const std::string& fileName, const cv::FileStorage& fSettings, const DefaultParameters& defaults) {
+void SaveCameraParametersToFile(const std::string& fileName, cv::FileStorage& fSettings) {
     std::ofstream outFile(fileName);
     if (!outFile.is_open()) {
         std::cerr << "Error: Cannot open file for writing: " << fileName << std::endl;
